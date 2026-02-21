ac33b81fa65aa209a3ad300b4515d6f75c4cd6c3
moncef1818 <monomoncef7@gmail.com>
2026-02-21 01:37:11 +0100

    fix allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -15,8 +15,7 @@ import os
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['django-chanels-group-chat.onrender.com']
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
